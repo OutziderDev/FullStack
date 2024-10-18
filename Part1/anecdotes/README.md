c1f4d520465fcc5c8c5ceda957092bc794711462
Luis <luisbarrios0110@gmail.com>
2024-10-18 17:23:10 -0500

    Add Part1: 1.13 and 1.14, Final Version of Anecdotes with Popular anecdote View

--- Part1/anecdotes/README.md
+++ Part1/anecdotes/README.md
@@ -5,4 +5,5 @@ Carpetas de desarrollo de ejercicios del curso FullStack Para la Seccion 1 - Ane
 Ejercicios Desarrollados:
 
 - 1.12 Anecdotes (Paso 1, Expandiendo aplicacion con boton para generar anecdotas aletorias)
-
+- 1.13 Anecdotes (paso 2, Implementando los votos por anecdotas y <button> de votos)
+- 1.14 Anecdotes (paso 3, Version Final, agregando vista de anecdota mas popular)
--- Part1/anecdotes/src/App.jsx
+++ Part1/anecdotes/src/App.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react"
 
+//Componentes 
+const Button = ({onClick,text}) => <button onClick={onClick}>{text}!</button>
+
+const MostVoteAnecdote = ({points,anecdotes}) =>{
+  let maxPoint =0;
+  let numAnectote = 0; 
+
+  for (let key in points) {
+    if(points[key] > maxPoint){
+      maxPoint = points[key];
+      numAnectote = key;
+    }
+  }
+  if(maxPoint === 0 ) return <div>Not vote yet</div>
+  return(
+    <div>{anecdotes[numAnectote]} <br />
+     With: {maxPoint} votes.
+    </div>
+  )
+}
+
 function App() { 
   //Variables
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [points,setPoints ]= useState({0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0})
+
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -15,16 +38,29 @@ function App() {
   ]
   //Funciones
   const handleNumberChange = () =>{
-    const randomNumer = Math.floor(Math.random()*8);
-    //console.log(randomNumer);
+    const randomNumer = Math.floor(Math.random()*anecdotes.length);
+    //console.log(points)
     setSelected(randomNumer);
+  }
+
+  const handleVoteIncrement = () =>{
+    const newVoteObject = points
+    newVoteObject[selected] += 1
+    setPoints(newVoteObject)
   }
 
   return (
     <>
-      <div>{anecdotes[selected]}</div>
+      <div>{anecdotes[selected]}
+        <br /> This anecdote has: {points[selected]} votes
+      </div>
+      
       <br />
-      <button onClick={handleNumberChange}>Next Anecdote</button>
+      <Button onClick={handleVoteIncrement} text={"Vote for this!"}/>
+      <Button onClick={handleNumberChange} text={"Next Anecdote"}/>
+      <hr/>
+      <p><b>Anecdote with most votes</b></p>
+      <MostVoteAnecdote points={points} anecdotes={anecdotes}/>
     </>
   )
 }

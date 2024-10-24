e60c4f332558086185e8b3e1e2ce659e006e7255
Luis <luisbarrios0110@gmail.com>
2024-10-24 14:26:49 -0500

    Add Part2: 2.1 Course Info Step 6, integrate all in one component <Course>

--- Part2/CourseInfo2.0/src/App.jsx
+++ Part2/CourseInfo2.0/src/App.jsx
@@ -2,48 +2,42 @@ const Header = ({ course }) => <h1>{course}</h1>
 
 const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+const Content = ({ parts}) =>  parts.map((part,index) => <Part key={index} part={part}/>) 
+
+const Course = ({course}) => {
+  //console.log(course);
+  //console.log(course.parts.length);
+  return(
+    <>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+    </>
+  )
+}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    id:1, 
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App

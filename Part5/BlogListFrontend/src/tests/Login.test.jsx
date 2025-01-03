3e4355ef89ffd5e62ec2191b29ed6dba7d90dd75
Luis <luisbarrios0110@gmail.com>
2025-01-02 23:43:51 -0500

    Fix error in 5.16

--- Part5/BlogListFrontend/src/tests/Login.test.jsx
+++ Part5/BlogListFrontend/src/tests/Login.test.jsx
@@ -2,6 +2,13 @@ import userEvent from "@testing-library/user-event"
 import {render,screen} from "@testing-library/react"
 import Login from "../Components/Login"
 
+HTMLFormElement.prototype.requestSubmit = function () {
+  if (this.dispatchEvent) {
+    const event = new Event('submit', { bubbles: true, cancelable: true });
+    this.dispatchEvent(event);
+  }
+}
+
 test('test login call submit ', async () => { 
   const handleLogin = vi.fn()
   const handleUser = vi.fn()
@@ -12,9 +19,6 @@ test('test login call submit ', async () => {
 
   const inputuser = screen.getByTestId('inputUser')
   const inputpass = screen.getByTestId('inputPass')
-  //screen.debug(inputuser)
-  //screen.debug(inputpass)
-  
   const sendButton = screen.getByText('Login')
   //screen.debug(sendButton)
 
@@ -23,5 +27,8 @@ test('test login call submit ', async () => {
   await user.click(sendButton)
 
   expect(handleLogin.mock.calls).toHaveLength(1)
-  //expect(handleLogin.mock.calls[0][0].content).toBe("admin")
+  //console.log('handlemock:',handleLogin.mock.calls)
+  expect(handleLogin).toHaveBeenCalledTimes(1)
+  
+  //expect(handleLogin).toHaveBeenCalledWith({ username: 'admin', password: '123' })
 })
--- Part5/BlogListFrontend/testSetup.js
+++ Part5/BlogListFrontend/testSetup.js
@@ -2,6 +2,13 @@ import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
+HTMLFormElement.prototype.requestSubmit = function() {
+  if (this.dispatchEvent) {
+    const event = new Event('submit', { bubbles: true, cancelable:true})
+    this.dispatchEvent(event)
+  }
+}
+
 afterEach(() => {
   cleanup()
 })

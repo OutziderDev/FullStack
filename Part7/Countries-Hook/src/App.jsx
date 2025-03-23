11a110c09adc5d27b8a27b37c94c59bc5114948f
Luis <luisbarrios0110@gmail.com>
2025-03-23 17:44:25 -0500

    Fix: Cambiando la interfaz del proyecto de paises

--- Part7/Countries-Hook/src/App.jsx
+++ Part7/Countries-Hook/src/App.jsx
@@ -1,6 +1,5 @@
 import  { useState } from 'react'
-import { useField } from './hooks'
-import { useCountry } from './hooks'
+import { useField, useCountry } from './hooks'
 import Country from './components/Country'
 import StarBorder from './components/StarBorder'
 
@@ -10,30 +9,38 @@ const App = () => {
   const country = useCountry(name)
 
   const fetch = (e) => {
-    e.preventDefault()
+    e.preventDefault() 
     setName(nameInput.value)
   }
 
   return (
-    <div className='container mx-auto max-w-2xl'>
-      <h1 className='text-white uppercase text-center text-7xl m-10 text-wrap font-bold'>Detalles de Paises</h1>
-      <form className='' onSubmit={fetch}>
-        <StarBorder
-          as="div"
-          className="w-full"
-          color="white"
-          speed="4s"
-        >
-          <div className='flex justify-between'>  
-            <input
-              {...nameInput} 
-              placeholder="Escribe tu país: Mexico..."
-              className="bg-transparent border-none outline-none text-white placeholder-gray-400 w-full p-2"
-            />
-            <button className=' text-white px-4 py-2 rounded hover:cursor-pointer hover:bg-amber-50/10'>Buscar</button>
-          </div>
-        </StarBorder>
-      </form>
+    <div className=''>
+      <div className=' relative w-full h-full'>
+
+        <div className=" absolute bg-[url('/fondo.webp')] inset-0 bg-cover bg-center w-full blur-md grayscale-50"></div>
+
+        <h1 className='text-white font-primary uppercase text-center text-6xl pt-20 text-wrap font-bold -skew-3'>GeoMundi Digital </h1>
+
+        <p className='text-center text-cyan-300 backdrop-blur-sm select-none  font-bold my-8 py-4 text-3xl -skew-3 animate-pulse'>Revisa informacion del pais de tu curiosidad</p>
+      
+        <form className='max-w-6xl mx-auto pb-10' onSubmit={fetch}>
+          <StarBorder
+            as="div"
+            className="w-full"
+            color="white"
+            speed="4s"
+          >
+            <div className='flex justify-between'>  
+              <input
+                {...nameInput} 
+                placeholder="Escribe tu país: Mexico..."
+                className="bg-transparent border-none outline-none text-white placeholder-gray-400 w-full p-2"
+              />
+              <button className=' text-white px-4 py-2 rounded hover:cursor-pointer hover:bg-amber-50/10'>Buscar</button>
+            </div>
+          </StarBorder>
+        </form>
+      </div>
 
       <Country country={country} />
     </div>

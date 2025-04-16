a352867d1b0831eefc193d427c9a9a7329905a4c
Luis <luisbarrios0110@gmail.com>
2025-04-16 15:58:06 -0500

    Add Part9: 9.6 Eslint with Ts (Add eslint v9 and rules for check ts code)

---
+++ Part9/base-typescrip/eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      },
+      globals: {
+        process: 'readonly',
+        console: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',      
+    }
+  }
+];
